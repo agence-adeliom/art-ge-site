42d46af574f4bfe154915dc7c082d205596822a6
Jean-David Daviet <jean-david.daviet@adeliom.com>
2023-09-22 18:03:55 +0200

    build(deps): add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./assets/**/*.js",
+    "./assets/**/*.ts",
+    "./assets/**/*.tsx",
+    "./templates/*.html.twig",
+    "./templates/**/*.html.twig",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
