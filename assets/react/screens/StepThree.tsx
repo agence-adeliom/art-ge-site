fbee6e4e6732f1fa2edf37937bfd07076e4d7d2a
hschuhler <hugo.schuhler@adeliom.com>
2023-12-07 17:16:08 +0100

    feat(step 3): array and map

--- assets/react/screens/StepThree.tsx
+++ assets/react/screens/StepThree.tsx
@@ -4,6 +4,8 @@ import { Text } from '@components/Typography/Text'
 import { Button } from '@components/Action/Button'
 import { YesNoCard } from '@components/Fields/YesNoCard';
 
+const cardClassName = "col-span-1 w-full p-4 cursor-pointer flex gap-4 flex items-center border border-neutral-200 group trans-default lg:hover:bg-tertiary-200 is-active:border-primary-600 is-active:bg-primary-50 peer"
+
 const StepThree = ({isRestaurant, setIsRestaurant, setIsGreenSpace, isGreenSpace, nextStep} : {
     isRestaurant: string,
     setIsRestaurant: Function,
@@ -11,22 +13,38 @@ const StepThree = ({isRestaurant, setIsRestaurant, setIsGreenSpace, isGreenSpace
     isGreenSpace: string,
     nextStep: Function
 }) => {
-    const cardClassName = "col-span-1 w-full p-4 cursor-pointer flex gap-4 flex items-center border border-neutral-200 group trans-default lg:hover:bg-tertiary-200 is-active:border-primary-600 is-active:bg-primary-50 peer"
+
+
+    const arrayQuestions = [
+        {
+            text: `Disposez-vous d'un espace vert, d'un espace extérieur de plus de 100m2 ?`,
+            choice: isRestaurant,
+            handleChoice: setIsRestaurant,
+            id: 'restaurant'
+        },
+        {
+            text: `Proposez-vous une offre de restauration (panier, pique-nique, restaurant…) ?`,
+            choice: isGreenSpace,
+            handleChoice: setIsGreenSpace,
+            id: 'greenSpace'
+        },
+    ]
     return (
         <>
             <Heading variant="display-4">Vous êtes...</Heading>
             <Text className="mt-6" color="neutral-500" weight={400} size="sm">Indiquez l’activité de votre établissement touristique.</Text>
+
             
-            <div className="mt-8">
-                <Text className="mb-4" color="neutral-700" weight={400} size="sm">Proposez-vous une offre de restauration (panier, pique-nique, restaurant…) ?</Text>
-
-                <YesNoCard handleChoice={setIsRestaurant} choice={isRestaurant} className={cardClassName} id="restaurant"></YesNoCard>            
-            </div>
-            <div className="mt-8">
-                <Text className="mb-4" color="neutral-700" weight={400} size="sm">Disposez-vous d'un espace vert, d'un espace extérieur de plus de 100m2 ?</Text>
-
-                <YesNoCard handleChoice={setIsGreenSpace} choice={isGreenSpace} className={cardClassName} id="greenSpace"></YesNoCard>            
-            </div>
+            {arrayQuestions.map((item, index) => {
+                return (
+                    <div className="mt-8" key={index}>
+                        <Text className="mb-4" color="neutral-700" weight={400} size="sm">
+                            {item.text}
+                        </Text>
+                        <YesNoCard handleChoice={item.handleChoice} choice={item.choice} className={cardClassName} id={item.id}></YesNoCard>            
+                    </div>
+                )
+            })}
             
             <Button 
                 size="lg" 
