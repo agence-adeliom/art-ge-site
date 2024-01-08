f307968e1ff383e7d0d3d6af47a13fb31ddcc578
Jean-David Daviet <jean-david.daviet@adeliom.com>
2024-01-08 15:43:27 +0100

    add test command to debug on ssh server

---
+++ src/Command/Helpers/TestEmailConfirmationMessageCommand.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Command\Helpers;
+
+use App\Message\GenerateReponsePDF;
+use App\Repository\ReponseRepository;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Messenger\MessageBusInterface;
+
+#[AsCommand(
+    name: 'test:email:send-confirmation',
+    description: 'Send a confirmation email for reponse 2',
+)]
+class TestEmailConfirmationMessageCommand extends Command
+{
+    public function __construct(
+        private readonly MessageBusInterface $messageBus,
+        private readonly ReponseRepository $reponseRepository,
+    ) {
+        parent::__construct();
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        /* @phpstan-ignore-next-line */
+        $this->messageBus->dispatch(new GenerateReponsePDF($this->reponseRepository->find(2)));
+
+        return Command::SUCCESS;
+    }
+}
