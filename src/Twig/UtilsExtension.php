64750ed9c55ecaa2c3f5f05b10705b04f0c047f3
Jean-David Daviet <jean-david.daviet@adeliom.com>
2024-01-08 15:10:18 +0100

    remove code not needed anyore

--- src/Twig/UtilsExtension.php
+++ src/Twig/UtilsExtension.php
@@ -22,30 +22,4 @@ class UtilsExtension extends AbstractExtension
             new TwigFunction('display_percentage', $this->percentagePresenter->displayPercentageWithSign(...)),
         ];
     }
-
-    public function displayPercentage(float | int | Reponse $amount, float | int $total = 100): string
-    {
-        if ($amount instanceof Reponse) {
-            $percentage = $this->getReponsePercentage($amount);
-        } else {
-            $percentage = $this->getPercentage($amount, $total);
-        }
-
-        return $percentage . '%';
-    }
-
-    private function getPercentage(float | int $amount, float | int $total): int
-    {
-        if (0 === $total) {
-            // just in case division by 0
-            return 0;
-        }
-
-        return (int) round($amount / $total * 100);
-    }
-
-    private function getReponsePercentage(Reponse $reponse): int
-    {
-        return $this->getPercentage($reponse->getPoints(), $reponse->getTotal());
-    }
 }
