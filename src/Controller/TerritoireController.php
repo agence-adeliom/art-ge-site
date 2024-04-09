088b3720652e339524bd036e99d0418e22bbf5b0
Jean-David Daviet <jean-david.daviet@adeliom.com>
2024-04-09 11:31:54 +0200

    dont allow access to other terrioire if already connected

--- src/Controller/TerritoireController.php
+++ src/Controller/TerritoireController.php
@@ -4,11 +4,13 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Entity\Territoire;
 use App\Exception\TerritoireNotFound;
 use App\Repository\TerritoireRepository;
 use Symfony\Bridge\Twig\Attribute\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Core\Exception\BadCredentialsException;
 
 class TerritoireController extends AbstractController
 {
@@ -25,6 +27,13 @@ class TerritoireController extends AbstractController
     public function __invoke(
         string $identifier,
     ): array {
+        $user = $this->getUser();
+        if ($user instanceof Territoire && $user->getSlug() !== 'grand-est') {
+            if ($user->getSlug() !== $identifier) {
+                throw new BadCredentialsException('Invalid credentials.');
+            }
+        }
+
         $territoire = $this->territoireRepository->getOneByUuidOrSlug($identifier);
 
         if (!$territoire) {
